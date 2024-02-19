ee037719d9c584d0fe727a8149d316b14f88f65c
vaibhavugale <ugalevaibhav00@gmail.com>
2024-02-19 13:56:58 +0530

    add search filter in category

--- src/components/pages/Category/Category.js
+++ src/components/pages/Category/Category.js
@@ -5,9 +5,12 @@ import { useParams } from "react-router-dom";
 import { DataContext } from "../../../Context";
 import { DNA } from "react-loader-spinner";
 import toast from "react-hot-toast";
+import RangeSlider from 'react-range-slider-input';
 
 const Category = () => {
   const [itemList, setItemLIst] = useState([]);
+  const [filterValue , setFilterValue] = useState();
+  const [filterData,setFilterData] = useState([]);
   const {catName} = useParams();
   const [cardData, setCardData] = useState();
   const [readMore , setReadMore] = useState(true);
@@ -25,11 +28,39 @@ const Category = () => {
       );
       const data = await res.json();
       setItemLIst(data);
+      setFilterData(data);
     };
     getProduct();
   }, []);
+
+  const debounce= (func,delay) =>{
+    let timeoutId;
+    return function (...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
+
+  const filter = (e) =>{
+    if(e.target.value == 10){
+      setFilterData(itemList);
+      setFilterValue(+e.target.value);
+
+      return;
+    }
+     const filterResult = itemList.filter((item)=> +item?.price <= +e.target.value)
+     setFilterData(filterResult);
+      setFilterValue(+e.target.value);
+   
+  }
   return (
     <div className=" relative">
+     <div className="  relative flex mt-6 ml-5">
+        <p>0</p> <input  onChange={debounce(filter,100)} type="range" min={10} max={1000}   /> <p>10000</p>
+        <p className="  absolute bg-white top-[25px] shadow-xl rounded-md  ">{filterValue}</p>
+     </div>
       <motion.div
         initial={{ x: "-5%", opacity: 0 }}
         animate={{ x: 0, opacity: "100%" }}
@@ -37,7 +68,7 @@ const Category = () => {
         transition={{ duration: 0.5 }}
         className="flex overflow-scroll justify-center items-center mt-8 mb-4 gap-5 flex-wrap max-h-[90vh] max-w-[1080px] mx-auto"
       >
-        { itemList.length===0 ?
+        { filterData.length===0 ?
         ( <div className=" w-[100vw] h-[100vh] flex justify-center items-center" >
                 <DNA
           visible={true}
@@ -49,7 +80,7 @@ const Category = () => {
           />
           
      </div>):
-      itemList.map((item) => (
+     filterData.map((item) => (
           <ItemCard key={item?.id} itemData={item} setCardData={setCardData} />
         ))   }
       </motion.div>
